dc406ed91ea7dd1e06239b82f275ea6d16e47235
Ishaan Kesarwani <kesarwaniishaan4@gmail.com>
2024-06-20 23:38:32 +0530

    initial

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main() {
+    std::cout << "heyyyy!!!" << "\n";
+
+    // << "\n" is used to enter to a new line, sometimes << std::endl is also used!
+
+    std::cout << "heyyyy!!!" << std::endl;
+
+    std::cout << "heyyyy!!!";
+    return 0;
+
+}
+
+
+// if you do not want to write std:: again and again, then simply write "using namespace std;" below #include
