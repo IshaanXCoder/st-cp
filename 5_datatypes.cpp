773ed35e37b9c43d31ffdbf4ca4f0c377b6264e9
Ishaan Kesarwani <kesarwaniishaan4@gmail.com>
2024-06-22 00:22:52 +0530

    datatypes

---
+++ 5_datatypes.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// int main(){
+//     float x;
+//     cin >> x;
+//     cout << "the value of x, which is a decimal is : " << x;
+//     l
+//     return 0;
+
+// }
+
+// int main(){
+//     string s1, s2;
+//     cin >> s1 >> s2;
+//     cout << "First name : " << s1 << " and, last name is " << s2;  
+//     return 0;
+
+// }
+
+
+
+int main(){
+
+    string x;
+    getline(cin, x);
+    cout << "My name is : " << x;
+    return 0;
+}
