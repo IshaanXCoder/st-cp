f2c35a40f849749b1351ba8e3a94b5a4badbc820
Ishaan Kesarwani <kesarwaniishaan4@gmail.com>
2024-06-21 22:38:29 +0530

    latest

---
+++ 4.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main () {
+    int x, y;
+    cin >> x >> y;
+    cout << "the value of x is " << x << " and the value of y is " << y;
+    return 0;
+}
+
