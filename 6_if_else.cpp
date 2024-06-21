5dd8d1aba276308ab13cc5b9c1d23ee992b349ef
Ishaan Kesarwani <kesarwaniishaan4@gmail.com>
2024-06-22 00:49:09 +0530

    marks/grade

---
+++ 6_if_else.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int x;
+    cin >> x;
+    if( x>=18 ) {
+        cout << "youre an adult" ;
+        return 0;
+    }
+    else {
+        cout << "baccha he";
+        return 0;
+    }
+}
+
