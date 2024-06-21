1f6cc6358eb477db3b866bf276c1fff40c21cbbd
Ishaan Kesarwani <kesarwaniishaan4@gmail.com>
2024-06-21 17:34:09 +0530

    input_output

---
+++ 3.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+    int x, y;
+    cin >> x >> y;
+    cout << "the value of x is " << x << " and the value of y is " << y;
+    return 0;
+}
