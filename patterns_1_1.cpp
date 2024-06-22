088f469a4d4c2d897f17eb69738d1d4adefa974c
Ishaan Kesarwani <kesarwaniishaan4@gmail.com>
2024-06-23 01:26:03 +0530

    problems

--- patterns_1_1.cpp
+++ patterns_1_1.cpp
@@ -1,5 +1,17 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 
-void fx(){
+void fucntion(int n){
     
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            std::cout << "*";
+        }
+            std::cout << std::endl;
+    }
+}
+
+int main(){
+    int n;
+    std::cin >> n;
+    fucntion(n);
 }
