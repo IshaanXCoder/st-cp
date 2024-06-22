ec096ca689360ef79dd58dea0e2b8f96a5416f27
Ishaan Kesarwani <kesarwaniishaan4@gmail.com>
2024-06-22 12:33:26 +0530

    arr

---
+++ 7_switch_cases.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+
+int main(){
+    int day;
+    std:: cin >> day;
+    switch (day)
+    {
+    case 1: 
+    std:: cout << "monday";
+        break;
+    
+    case 2:
+    std:: cout << "tuesday";
+        break;
+    case 3:
+    std:: cout << "wednesday";
+        break;
+    case 4:
+    std:: cout << "thursday";
+        break;
+    case 5:
+    std:: cout << "friday";
+        break;
+    case 6:
+    std:: cout << "saturday";
+        break;
+    case 7:
+    std:: cout << "sunday";
+        break;
+    default:
+    std:: cout << "nahhh mazak na kar";
+    //defualt is used when all the conditions fail!
+
+
+    }
+return 0;
+}
