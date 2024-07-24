81fc2afd604f34b18aaf285e486520f5029bf945
Ishaan Kesarwani <kesarwaniishaan4@gmail.com>
2024-07-25 01:03:51 +0530

    .

---
+++ 6_Sorting/01_selectionSorting.cpp
@@ -0,0 +1,8 @@
+#include<bits/stdc++.h>
+
+// Selection sorting --->> Selecitn the minimums and swapping it 
+// exmaple fi 10 20 9 1 14 is an array
+// after sorting -->>1 20 9 10 14
+// again after sorting -->>1 9 20 10 14
+// again after sorting -->>1 9 10 20 14
+// again after sorting -->>1 9 10 14 20
