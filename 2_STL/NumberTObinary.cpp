537e5ce330fda1d42d4702907bc10389e08fc4e5
Ishaan Kesarwani <kesarwaniishaan4@gmail.com>
2024-08-22 14:32:49 +0530

    push

---
+++ 2_STL/NumberTObinary.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+int main(){
+    int a;
+    std::cout<<"Write the number : ";
+    std::cin>>a;
+    std::vector<int> myArr;
+    std::string b;
+    for(int i=a;a>0;--i){
+
+        myArr.push_back(a%2);
+        a=a/2;
+    }
+
+        for (int i = myArr.size() - 1; i >= 0; --i) {
+        std::cout << myArr[i];
+    }
+
+}
