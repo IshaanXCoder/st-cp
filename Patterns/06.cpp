734286b6b7942c243dd57c91c6fc6ab4f6328080
Ishaan Kesarwani <kesarwaniishaan4@gmail.com>
2024-08-28 11:56:22 +0530

    push

--- Patterns/06.cpp
+++ Patterns/06.cpp
@@ -3,7 +3,7 @@
 int main(){
     for(int i=0; i<5; i++){
         for(int j=0; j<=4-i; j++){ 
-            std::cout << "*";
+            std::cout << j+1;
         }
         std::cout << std::endl;
     }
