a823f246dbbea299072575e91d98c56b19ab9a98
Ishaan Kesarwani <kesarwaniishaan4@gmail.com>
2024-08-28 09:15:35 +0530

    patterns

--- Patterns/01.cpp
+++ Patterns/01.cpp
@@ -1,2 +1,12 @@
 // SIMPLE RULE : Run 2 loops, loop 1 from i to n (number of lines) and seoncd loops to find a relation between numb of rows and columns
 
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    for(int i=0;i<4;i++){
+        for(int j=0;j<4;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+}
---
+++ Patterns/05.cpp
@@ -0,0 +1,11 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    for(int i=0;i<=5;i++){
+        for(int j=5;j=i;j--){
+            std::cout<<"*";
+        }
+        std::cout<<std::endl;
+    }
+}
