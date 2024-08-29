fff4ae6b0b3a01689aaa2c0a0b700b86c0d7adb9
Ishaan Kesarwani <kesarwaniishaan4@gmail.com>
2024-08-29 23:09:58 +0530

    pyramid

--- Patterns/07.cpp
+++ Patterns/07.cpp
@@ -1,6 +1,23 @@
 #include<bits/stdc++.h>
 int main(){
+
+    int n;
+    std::cin>>n;
     for(int i=0;i<n;i++){
-        for(int j=0;j<n)
+        for(int j=n-1;j>=0;j--){
+            std::cout<<" ";
+
+        }
+
+        for(int j=1;j<=2i+1;j++){
+            std::cout<<"*";
+        }
+
+        for(int j=n-1;j>=0;j--){
+            std::cout<<" ";
+        }
+        std::cout<<std::endl;
+        
     }
+    return 0;
 }
