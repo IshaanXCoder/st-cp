d920ea7f0bbd96ff5b50b3643fed79faa01bca09
Ishaan Kesarwani <kesarwaniishaan4@gmail.com>
2024-09-07 12:43:49 +0530

    pointerss

---
+++ pointer.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+int main(){
+    // what is a pointer ----> a type of datatype which holds address of other datatypes
+
+    int a=3;
+    int* b= &a;
+
+    // create a pointer variable b which olds the adress of a
+    // & is address of operator and * is deferencing operator
+
+    std::cout<<"address of a is : "<<&a<<std::endl;
+    std::cout<<"address of a is : "<<b<<std::endl;
+s
+
+
+    std::cout<<"value of b is : "<<*b;
+
+
+}
