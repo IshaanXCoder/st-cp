7a69fcebdac0219ae966ab561192550f9d6319fa
Ishaan Kesarwani <kesarwaniishaan4@gmail.com>
2024-09-08 22:52:00 +0530

    Commit for September 8th at 10:52 PM

--- guessNum.cpp
+++ guessNum.cpp
@@ -12,6 +12,8 @@ int main() {
     cout << "Welcome to the Guess the Number Game!" << endl;
     cout << "I have selected a number between 1 and 100. Can you guess it?" << endl;
 
+    
+
     do {
         cout << "Enter your guess: ";
         cin >> guess;
