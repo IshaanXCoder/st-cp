02342f556431543179252de4546b631dd239a7c1
Ishaan Kesarwani <kesarwaniishaan4@gmail.com>
2024-09-10 22:52:00 +0530

    Your commit message

--- pointer.cpp
+++ pointer.cpp
@@ -1,19 +1,28 @@
-#include<iostream>
-int main(){
-    // what is a pointer ----> a type of datatype which holds address of other datatypes
+#include <iostream>
+using namespace std;
 
-    int a=3;
-    int* b= &a;
+int main() {
+    // 1. Declare an integer variable
+    int num = 10;
+    
+    // 2. Declare a pointer to int and initialize it with the address of 'num'
+    int *ptr = &num;  // '&' gives the memory address of 'num'
+    
+    // 3. Output the value of 'num' and the memory address stored in 'ptr'
+    cout << "Value of num: " << num << endl; // Value of num is 10
+    cout << "Memory address of num: " << &num << endl; // Prints address of 'num'
+    cout << "Pointer ptr stores: " << ptr << endl; // Pointer stores the same address
 
-    // create a pointer variable b which olds the adress of a
-    // & is address of operator and * is deferencing operator
-
-    std::cout<<"address of a is : "<<&a<<std::endl;
-    std::cout<<"address of a is : "<<b<<std::endl;
-
-
-
-    std::cout<<"value of b is : "<<*b;
-
-
+    // 4. Dereference the pointer to get the value stored at that memory address
+    cout << "Value at the address stored by ptr: " << *ptr << endl; // Dereferencing pointer, prints 10
+    
+    // 5. Modify the value of 'num' using the pointer
+    *ptr = 20;  // Changes the value at the memory location 'ptr' points to
+    cout << "New value of num after modification via pointer: " << num << endl; // Prints 20
+    
+    // 6. Demonstrate a null pointer
+    int *nullPtr = nullptr;  // nullptr indicates that the pointer is not assigned any memory address
+    cout << "Null pointer value: " << nullPtr << endl;  // Outputs 0 (null)
+    
+    return 0;
 }
