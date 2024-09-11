c7219e9dc2b97161bc8b8e7ecbbbe85c1495e3d6
Ishaan Kesarwani <kesarwaniishaan4@gmail.com>
2024-09-11 17:16:58 +0530

    gaemee

---
+++ guessNum.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int main() {
+    srand(time(0));
+    int randomNumber = rand() % 100 + 1;
+    int guess;
+    int attempts = 0;
+
+    cout << "Welcome to the Guess the Number Game!" << endl;
+    cout << "I have selected a number between 1 and 100. Can you guess it?" << endl;
+
+    do {
+        cout << "Enter your guess: ";
+        cin >> guess;
+        attempts++;
+
+        if (guess > randomNumber) {
+            cout << "Too high! Try again." << endl;
+        } else if (guess < randomNumber) {
+            cout << "Too low! Try again." << endl;
+        } else {
+            cout << "Congratulations! You guessed the number in " << attempts << " attempts." << endl;
+        }
+    } while (guess != randomNumber);
+
+    return 0;
+}
